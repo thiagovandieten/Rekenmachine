b8a0b3aaaffa8f1d997dd1fdfdb0454e3390be2d
Thiago van Dieten <nktakumi@gmail.com>
2020-12-06 14:04:23 +0100

    add click event to reflect button content on display

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,27 @@ namespace Rekenmachine
         public MainWindow()
         {
             InitializeComponent();
+            bindNumberButtons();
+        }
+
+        private void bindNumberButtons()
+        {
+            Num0.Click += NumButton_Click;
+            Num1.Click += NumButton_Click;
+            Num2.Click += NumButton_Click;
+            Num3.Click += NumButton_Click;
+            Num4.Click += NumButton_Click;
+            Num5.Click += NumButton_Click;
+            Num6.Click += NumButton_Click;
+            Num7.Click += NumButton_Click;
+            Num8.Click += NumButton_Click;
+            Num9.Click += NumButton_Click;
+        }
+
+        private void NumButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button buttonPressed = (Button)sender;
+            Display.Text += buttonPressed.Content;
         }
     }
 }
